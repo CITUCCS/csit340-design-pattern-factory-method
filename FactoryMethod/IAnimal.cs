99a2135688798e1c303dff377dd3814d1cab6940
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-11-08 15:35:18 +0800

    Implement

--- FactoryMethod/IAnimal.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FactoryMethod
-{
-    public interface IAnimal
-    {
-        public string Render();
-    }
-}
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,37 +1,10 @@
 ﻿using FactoryMethod;
+using FactoryMethod.Animal.Factories;
 
-int ParkAnimalLimit = 10;
-GeneratePark();
+ParkGenerator pg = new MayorParkGenerator();
+Console.WriteLine("Mayor---");
+pg.Generate();
 
-
-void GeneratePark()
-{
-    Console.WriteLine("Generating park!");
-    Random r = new Random();
-    
-    var numAnimals = 0;
-    while (numAnimals < ParkAnimalLimit)
-    {
-        IAnimal animal;
-        var rInt = r.Next(0, 2);
-
-        switch (rInt)
-        {
-            case 0:
-                animal = new Cow();
-                break;
-            case 1:
-                animal = new Bird();
-                break;
-            case 2:
-                animal = new Snake();
-                break;
-            default:
-                animal = new Cow();
-                break;
-        }
-
-        numAnimals++;
-        Console.WriteLine(animal.Render());
-    }
-}
+pg = new ViceMayorParkGenerator();
+Console.WriteLine("Vice Mayor---");
+pg.Generate();
